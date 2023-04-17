3ce74ee80c35c5d2db4e1975d96ccc823c87d226
Phillips, Isabella (Capgemini) <isabella.phillips@capgemini.com>
2023-04-17 12:34:36 +0100

    added card counter functionality

---
+++ Assets/Scripts/CardCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardCounter : MonoBehaviour
+{
+
+    public TextMeshProUGUI cardCounter;
+    private int cardsLeft;
+    private MemMad memMad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        memMad = FindObjectOfType<MemMad>();
+    }
+
+    void Update()
+    {
+        cardsLeft = memMad.deck.Count;
+        cardCounter.text = "Cards Left: " + cardsLeft;
+    }
+}
