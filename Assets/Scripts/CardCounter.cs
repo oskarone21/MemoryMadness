269ffb5e8f2fa1b65a4e98ff4d48718b692925ba
OskarR21 <53127214+oskarone21@users.noreply.github.com>
2023-04-24 12:59:23 +0100

    Update Assets/Scripts/CardCounter.cs
    add private accessors

--- Assets/Scripts/CardCounter.cs
+++ Assets/Scripts/CardCounter.cs
@@ -11,7 +11,7 @@ public class CardCounter : MonoBehaviour
     private MemMad memMad;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         memMad = FindObjectOfType<MemMad>();
     }
