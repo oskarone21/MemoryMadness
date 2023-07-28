c27623688962c7429cacb1fa5b511feae4c8f358
OskarR21 <53127214+oskarone21@users.noreply.github.com>
2023-07-28 17:41:58 +0100

    got the 2x2/3x3/4x4 to work (sort of)

--- Assets/Scripts/MemoryMadnessController.cs
+++ Assets/Scripts/MemoryMadnessController.cs
@@ -1,29 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
-using Unity.Burst.CompilerServices;
-using System;
+using UnityEngine;
 
 public class MemoryMadnessController : MonoBehaviour
 {
     private CardCounter cardCounter;
-    public int gridSize = 4;
+    public int gridSize;
 
     public Sprite[] cardFaces;
     public GameObject cardPrefab;
 
-    public GameObject[] topPos;
-    public GameObject[] upperMiddlePos;
-    public GameObject[] lowerMiddlePos;
-    public GameObject[] bottomPos;
+    public GameObject[] firstRow;
+    public GameObject[] secondRow;
+    public GameObject[] thirdRow;
+    public GameObject[] fourthRow;
     public GameObject[] handPos;
+
+    public GameObject[] rows;
+    public GameObject[][] cards;
 
     private static readonly string[] suits = { "C", "D", "H", "S" };
     private static readonly string[] values = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
     public List<string> cardsInPlay = new List<string>();
 
     private List<string>[][] cardPositions;
+    private List<string>[] handPositions;
+
     public List<string> deck;
 
     private void AddCardTo(ICollection<string> cardList)
@@ -58,7 +62,7 @@ public class MemoryMadnessController : MonoBehaviour
 
         SetCardPositions();
         MemMadSort();
-        StartCoroutine(MemMadDeal(autoReshuffle: false));
+        StartCoroutine(MemMadDeal());
     }
 
     public void ReplaceCards()
@@ -95,7 +99,7 @@ public class MemoryMadnessController : MonoBehaviour
 
     private IEnumerator DealToPositions(IReadOnlyList<List<string>> cardLists, IReadOnlyList<GameObject> positions, bool isHandCard = false)
     {
-        for (int i = 0; i < cardLists.Count; i++)
+        for (int i = 0; i < gridSize; i++)
         {
             float zOffset = 0.03f;
             foreach (string card in cardLists[i])
@@ -112,78 +116,46 @@ public class MemoryMadnessController : MonoBehaviour
             }
         }
     }
-    
+
     public static List<string> GenerateDeck() => 
         suits.SelectMany(_ => values, (suit, value) => suit + value).ToList();
 
-    private IEnumerator MemMadDeal(bool lastDeal = false, bool autoReshuffle = true)
+    private IEnumerator MemMadDeal(bool lastDeal = false)
     {
-        //could create tempTopPos and do for loop to populate with records until reach gridsize
-        //else would have to re-do DealToPositions
-        GameObject[] tempTopPos = new GameObject[gridSize];
-        GameObject[] tempUpperMiddlePos = new GameObject[gridSize];
-        GameObject[] tempLowerMiddlePos = new GameObject[gridSize];
-        GameObject[] tempBottomPos = new GameObject[gridSize];
-        
-        for (int i = 0; i < gridSize; i++){
-            tempTopPos[i] = topPos[i];
-            tempUpperMiddlePos[i] = upperMiddlePos[i];
-            tempLowerMiddlePos[i] = lowerMiddlePos[i];
-            tempBottomPos[i] = bottomPos[i];
-        }
-
         if (lastDeal)
         {
-            yield return StartCoroutine(DealToPositions(cardPositions[0], topPos));
-            yield return StartCoroutine(DealToPositions(cardPositions[4], handPos));
+            yield return StartCoroutine(DealToPositions(cardPositions[0], rows[0].GetComponentsInChildren<Transform>().Skip(1).Select(t => t.gameObject).ToArray()));
+            yield return StartCoroutine(DealToPositions(cardPositions[gridSize], handPos));
         }
         else
         {
-            //yield return StartCoroutine(DealToPositions(cardPositions[0], topPos));
-            //yield return StartCoroutine(DealToPositions(cardPositions[1], upperMiddlePos));
-            //yield return StartCoroutine(DealToPositions(cardPositions[2], bottomPos));
-
-            //if (gridSize >= 2)
-            //{
-            //    yield return StartCoroutine(DealToPositions(cardPositions[0], tempTopPos));
-            //    yield return StartCoroutine(DealToPositions(cardPositions[1], tempUpperMiddlePos));
-            //} 
-            //if (gridSize >= 3)
-            //{
-            //    yield return StartCoroutine(DealToPositions(cardPositions[2], tempLowerMiddlePos));
-            //}
-            if (gridSize == 4)
+            for (int i = 0; i < gridSize; i++)
             {
-                yield return StartCoroutine(DealToPositions(cardPositions[0], tempTopPos));
-                yield return StartCoroutine(DealToPositions(cardPositions[1], tempUpperMiddlePos));
-                yield return StartCoroutine(DealToPositions(cardPositions[2], tempLowerMiddlePos));
-                yield return StartCoroutine(DealToPositions(cardPositions[3], tempBottomPos));
+                yield return StartCoroutine(DealToPositions(cardPositions[i], rows[i].GetComponentsInChildren<Transform>().Skip(1).Select(t => t.gameObject).ToArray()));
             }
 
-            //if (autoReshuffle)
-            //{
-            //    yield return StartCoroutine(DealToPositions(cardPositions[4], handPos, true));
-            //}
+            yield return StartCoroutine(DealToPositions(cardPositions[gridSize], handPos, true));
         }
     }
 
 
     private void MemMadSort()
     {
-        for (int i = 0; i < 4; i++)
+        for (int row = 0; row < gridSize; row++)
         {
-            AddCardTo(cardPositions[3][i]); // Bottom
-            AddCardTo(cardPositions[2][i]); // LowerMiddle
-            AddCardTo(cardPositions[1][i]); // UpperMiddle
-            AddCardTo(cardPositions[0][i]); // Top
+            for (int col = 0; col < gridSize; col++)
+            {
+                AddCardTo(cardPositions[row][col]);
+            }
         }
 
         for (int i = 0; i < 2; i++)
         {
-            AddCardTo(cardPositions[4][i]); // Hand
+            AddCardTo(cardPositions[gridSize][i]); // Hand
         }
     }
-    
+
+
     private void PlayCards()
     {
         deck = GenerateDeck();
@@ -191,33 +163,82 @@ public class MemoryMadnessController : MonoBehaviour
         MemMadSort();
         StartCoroutine(MemMadDeal());
     }
-    
+
     private void Start()
     {
-        SetCardPositions();
-        PlayCards();
-        StartCoroutine(DelayInitialDealFlag());
+        gridSize = Mathf.Clamp(gridSize, 2, 4); // ensure the grid size is within valid range
+
+        cardPositions = new List<string>[gridSize + 1][];
+        for (int i = 0; i < gridSize; i++)
+        {
+            cardPositions[i] = new List<string>[gridSize];
+            for (int j = 0; j < gridSize; j++)
+            {
+                cardPositions[i][j] = new List<string>();
+            }
+        }
+
+        cardPositions[gridSize] = new List<string>[2]; // this is for the hand
+        for (int i = 0; i < 2; i++)
+        {
+            cardPositions[gridSize][i] = new List<string>();
+        }
+
         cardCounter = FindObjectOfType<CardCounter>();
+
+        cards = new GameObject[gridSize][]; // adjust the cards array to match gridSize
+        for (int i = 0; i < gridSize; i++)
+        {
+            cards[i] = new GameObject[gridSize]; // adjust the card row to match gridSize
+        }
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                cards[i][j] = rows[i].transform.GetChild(j).gameObject;
+            }
+        }
+
+        foreach (GameObject row in rows.Take(gridSize))
+        {
+            row.SetActive(false);
+        }
+        foreach (GameObject[] cardRow in cards)
+        {
+            foreach (GameObject card in cardRow)
+            {
+                card.SetActive(false);
+            }
+        }
+
+        SetGridSize(gridSize); // set up the grid size
+        StartCoroutine(DelayInitialDealFlag()); // start the game with the set grid size
     }
+
 
     private void SetCardPositions()
     {
-        new[] { new List<string>(), new List<string>(), new List<string>() }, // Tops
-        new[] { new List<string>(), new List<string>(), new List<string>() }, // upperMiddles
-        new[] { new List<string>(), new List<string>(), new List<string>() }, // lowerMiddles
-        new[] { new List<string>(), new List<string>(), new List<string>() }, // Bottoms
-        new[] { new List<string>(), new List<string>() } // Hands
+        cardPositions = new List<string>[gridSize][];
 
-        // cardPositions = new List<string>[gridSize][];
+        for (int i = 0; i < gridSize; i++)
+        {
+            cardPositions[i] = new List<string>[gridSize];
 
-        // for (int i = 0; i < gridSize; i++){
-        //     cardPositions[i] = new List<string>[gridSize];
-        //     for (int j = 0; j < gridSize; j++){
-        //         cardPositions[i][j] = new List<string>();
-        //     }
-        // }
+            for (int j = 0; j < gridSize; j++)
+            {
+                cardPositions[i][j] = new List<string>();
+            }
+        }
+
+        // For hand positions
+        cardPositions[gridSize] = new List<string>[2];
+
+        for (int j = 0; j < 2; j++)
+        {
+            cardPositions[gridSize][j] = new List<string>();
+        }
     }
-
 
     private static void Shuffle<T>(IList<T> list)
     {
@@ -235,6 +256,69 @@ public class MemoryMadnessController : MonoBehaviour
         // Wait for 3 seconds to ensure the cards have been dealt
         yield return new WaitForSeconds(3);
     }
+
+    public void SetGridSize(int size)
+{
+    gridSize = Mathf.Clamp(size, 2, 4); // Ensure the grid size is within the valid range
+
+    // Initialize the cardPositions array to match the gridSize
+    cardPositions = new List<string>[gridSize + 1][];
+    for (int i = 0; i < gridSize; i++)
+    {
+        cardPositions[i] = new List<string>[gridSize];
+        for (int j = 0; j < gridSize; j++)
+        {
+            cardPositions[i][j] = new List<string>();
+        }
+    }
+
+    // Initialize the hand positions separately
+    cardPositions[gridSize] = new List<string>[2]; 
+    for (int i = 0; i < 2; i++)
+    {
+        cardPositions[gridSize][i] = new List<string>();
+    }
+
+    // Initialize the cards array to match the gridSize
+    cards = new GameObject[gridSize][]; 
+    for (int i = 0; i < gridSize; i++)
+    {
+        cards[i] = new GameObject[gridSize]; 
+    }
+
+    // Assign the card objects to the 2D array
+    for (int i = 0; i < gridSize; i++)
+    {
+        for (int j = 0; j < gridSize; j++)
+        {
+            cards[i][j] = rows[i].transform.GetChild(j).gameObject;
+        }
+    }
+
+    // Hide all rows and cards not in use
+    for (int row = 0; row < rows.Length; row++)
+    {
+        rows[row].SetActive(false);
+        for (int col = 0; col < rows[row].transform.childCount; col++)
+        {
+            rows[row].transform.GetChild(col).gameObject.SetActive(false);
+        }
+    }
+
+    // Show the rows and cards in use
+    for (int row = 0; row < gridSize; row++)
+    {
+        rows[row].SetActive(true);
+        for (int col = 0; col < gridSize; col++)
+        {
+            cards[row][col].SetActive(true);
+        }
+    }
+
+    // Play the cards to fill the grid
+    PlayCards();
+}
+
 }
 
 
